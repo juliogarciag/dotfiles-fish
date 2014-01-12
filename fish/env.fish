db5431908de5a7b3f3f937e439f68394f3ac1ec8
Julio García <julioggonz@gmail.com>
2014-01-12 14:21:00 -0500

    erased unused global vars

--- fish/env.fish
+++ fish/env.fish
@@ -33,7 +33,7 @@ set PATH "/Applications/Sublime Text.app/Contents/SharedSupport/bin" $PATH
 # Play Bins
 set PATH "$HOME/bin/play-2.1.3" $PATH
 
-# fpcalc
+# Random home binaries
 set PATH "$HOME/bin" $PATH
 
 # Neo4j
@@ -43,13 +43,9 @@ set PATH "$HOME/bin/neo4j/bin" $PATH
 # set PATH "$HOME/bin/datomic/bin" $PATH
 
 # AWS
-# set AWS_DEFAULT_REGION = 
+# set AWS_DEFAULT_REGION = ...
 # keyID:    AKIAIQDHMVGUHC4OGF6Q
 # Secret:   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
 
 set fish_greeting ""
 set fish_color_cwd "bbbbbb"
-
-set -x SERVER_IP 192.168.1.9
-set -x GOOGLE_CUSTOM_SEARCH_KEY "AIzaSyA5bBm5S3osGN0f8eQgRJu-qKrUf5OvusY"
-set -x GOOGLE_CUSTOM_SEARCH_CX "009926135480036780929:8i5a-la835q"
