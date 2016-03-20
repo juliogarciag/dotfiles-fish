b8ab70a19e171a3b0d34f2c89329fad5dfb5c424
juliogarciag <julioggonz@gmail.com>
2016-03-19 19:38:14 -0500

    feat(markdown): md uses Typora now

--- environment.fish
+++ environment.fish
@@ -9,7 +9,7 @@ set -x DOCKER_CERT_PATH "$HOME/.docker/machine/machines/default";
 set -x DOCKER_MACHINE_NAME "default";
 set -x EDITOR vim
 set -x VISUAL_EDITOR atom
-set -x MARKDOWN_EDITOR "MacDown"
+set -x MARKDOWN_EDITOR "Typora"
 set -x DEV $HOME/dev
 set -x DOTFILES $DEV/dotfiles
 set -x DOWNLOAD_LOCATION $HOME/Downloads
