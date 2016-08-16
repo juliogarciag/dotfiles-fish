97b9f4f0d6d18f31001b45cf60a4498a027e0633
Julio García <julioggonz@gmail.com>
2016-08-16 11:44:26 -0500

    feat(abbreviations): Added more git related abbreviations

--- abbreviations.fish
+++ abbreviations.fish
@@ -6,7 +6,10 @@ abbr -a   gm          git merge
 abbr -a   commit      git commit -m
 abbr -a   pull        git pull
 abbr -a   push        git push
+abbr -a   fetch       git fetch
 abbr -a   rebase      git rebase
+abbr -a   clone       hub clone
 abbr -a   b           bundle exec
 abbr -a   l           ls -lah
 abbr -a   pr          hub pull-request -m
+abbr -a   stash       git stash
